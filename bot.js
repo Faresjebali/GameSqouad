f7920b100e4b033bd45b155dfa4fb48f2ad51dbc
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-02 08:42:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -310,7 +310,17 @@ client.on('message', function(message) {
         }
     }
 });
+client.on('message', message => {
+if (message.content.startsWith("*kick")) {
+  if(!message.member.hasPermission('KICK_MEMBER')) return      message.channel.send('**Unfortunately you do not have permission**' );
+    var mention = message.mentions.members.first();
+    if(!mention) return message.channel.send("mention person");
 
+    mention.kick("By: " + message.author.tag);
+    
+    message.channel.send("This person got kicked from the server : " + mention.tag);
+};
+});
 
 
 
