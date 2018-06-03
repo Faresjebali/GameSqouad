b29406764e2873662de04d2d41afced373e17fe3
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-03 10:25:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var blacklisted = ["discord.gg", "discord.gg/", "https://discord.gg"];
+const PREFIX = "*"
 
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+bot.on("message", function(message) { // when a message is sent
+    if (message.author.equals(bot.user)) return; // if the message is sent by a bot, ignore
+
+    if (!message.content.startsWith(PREFIX)) return; // if the message doesn't contain PREFIX (*), then ignore
+
+    var args = message.content.substring(PREFIX.length).split(" "); // removes the prefix from the message
+    var command = args[0].toLowerCase(); // sets the command to lowercase (making it incase sensitive)
+    var mutedrole = message.guild.roles.find("name", "muted");
 
 client.on('message', message => {
     if (message.content === "*help") {
