3f82e1b4ee38547c65de3c053cf2d5735e0d793e
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-03 07:20:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -149,28 +149,47 @@ client.on('message', message => {
     }
 });
 //
-client.on("message", message => {
-    var prefix = "*";
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **You do not have permissions**');
-        var msg;
+client.on('message', message => {
+   if(!message.channel.guild) return;
+  var prefix = "*"
+if(message.content.startsWith(prefix + 'clear')) {
+if(!message.channel.guild) return message.channel.send('**This Command is Just For Servers**').then(m => m.delete(5000));
+if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**You Do not have permission** `ADMINISTRATOR`' );
+let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
+let request = `Requested By ${message.author.username}`;
+message.channel.send(`**Are You sure you want to clear the chat?**`).then(msg => {
+msg.react('✅')
+.then(() => msg.react('❌'))
+.then(() =>msg.react('✅'))
+
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`Chat will delete`).then(m => m.delete(5000));
+var msg;
         msg = parseInt();
-      
+
       message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
       message.channel.sendMessage("", {embed: {
-        title: "Done | Done",
+        title: "`` Chat Deleted ``",
         color: 0x06DF00,
-        description: "Messages successfully cleared",
         footer: {
-          text: "ThaWither#1360"
+
         }
       }}).then(msg => {msg.delete(3000)});
-                          }
 
-     
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Chat deletion cancelled**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
 });
+
 client.on('ready',  () => {
   console.log('By : ThaWither');
   console.log(`Logged in as * [ " ${client.user.username} " ] servers! [ " ${client.guilds.size} " ]`);
