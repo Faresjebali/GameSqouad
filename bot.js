6d2c1b60e9d09a013344cb5a24e0fa315f1f4f7f
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-05 07:49:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -450,7 +450,7 @@ client.on('message',message => {
   if(!args[0]) return message.channel.send("oof");
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-       }
+       
    
 });
 
