503d643479e12c5c1a538b910cd065a1f243f8e1
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-05 07:44:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -444,6 +444,15 @@ client.on('message', message => {
          message.channel.send({embed:embed});
                         }
                     });
+client.on('message',message => {
+	if(message.content.startswith('*purge*')){
+		if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
+  if(!args[0]) return message.channel.send("oof");
+  message.channel.bulkDelete(args[0]).then(() => {
+    message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
+              }
+         }
+  });
 
 
 
