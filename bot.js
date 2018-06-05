1b7ea1f3fbb5cd43f49849e04137c390e021297b
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-05 03:14:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -162,28 +162,63 @@ client.on('message', message => {
     }
 });
 //
-var prefix = "*";
-client.on('message', message => {
-    if(message.content == prefix + 'server') {
-        var servername = message.guild.name
-        var Owner = message.guild.owner
-        var Members = message.guild.memberCount
-        var id = message.guild.id
-        var Countryserver = message.guild.region
-        var Rooms = message.guild.channels.size
-        var Ranks = message.guild.roles
-        var Work = message.guild.createdAt
-        var Room = message.guild.defaultChannel
-        var server = new Discord.RichEmbed()
-        .setThumbnail(message.guild.iconURL)
-        .addField('Server Name', servername)
-        .addField('Members of the server', Members)
-        .addField('Server rooms', Rooms)
-        .addField('Basic Chat Room', Room)
-        .addField('Server owner', Owner)
-        .setColor('RANDOM')
+var prefix = "*"
+    if(message.content.startsWith(prefix + "stats")) {
+           let uptime = client.uptime;
 
-        message.channel.sendEmbed(server)
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let notCompleted = true;
+
+    while (notCompleted) {
+
+        if (uptime >= 8.64e+7) {
+
+            days++;
+            uptime -= 8.64e+7;
+
+        } else if (uptime >= 3.6e+6) {
+
+            hours++;
+            uptime -= 3.6e+6;
+
+        } else if (uptime >= 60000) {
+
+            minutes++;
+            uptime -= 60000;
+
+        } else if (uptime >= 1000) {
+            seconds++;
+            uptime -= 1000;
+
+        }
+
+        if (uptime < 1000)  notCompleted = false;
+
+    }
+let ms = 1000;
+let v1 = new Discord.RichEmbed()
+  v1.setTimestamp(new Date())
+  v1.setColor("RED")
+  v1.setDescription('***__ Collecting Data __***')
+  v1.setFooter("# | SenioR |") 
+let heroo = new Discord.RichEmbed()
+.setColor('RANDOM')
+.setTimestamp(new Date())
+.setThumbnail(client.user.avatarURL)
+.setAuthor(client.user.username,client.user.avatarURL)
+.addField("MyPrefix :",`**[ ${prefix} ]**`,true)
+.addField("Guilds :","**[ "+client.guilds.size+" ]**",true)
+.addField("Channels :","**[ "+client.channels.size+" ]**",true)
+.addField("Users :","**[ "+client.users.size+" ]**",true)
+.addField("MyName : ","**[ "+client.user.username+" ]**",true)
+.addField("MyID :","**[ "+client.user.id+" ]**",true)
+.addField("RamUsage :",`**[ ${(process.memoryUsage().rss / 1048576).toFixed()}MB ]**`,true)
+.addField("UpTime :",`**[** **Days:** \`${days}\` **Hours:** \`${hours}\` **Minutes:** \`${minutes}\` **Seconds:** \`${seconds}\` **]**`,true)
+.setFooter("SenioR TeaM. |")
+  message.channel.send({embed:v1}).then(m => m.edit({embed:heroo})),ms; 
     }
 });
 //
@@ -465,127 +500,6 @@ client.on('messageDelete', message => {
      channel.send({embed:embed});
 
 });
-client.on("message", message => {
-
-	if (message.content.startsWith('*ping')) {
-                            if(!message.channel.guild) return;
-                            var msg = `${Date.now() - message.createdTimestamp}`
-                            var api = `${Math.round(client.ping)}`
-                            if (message.author.bot) return;
-                        let embed = new Discord.RichEmbed()
-                        .setAuthor(message.author.username,message.author.avatarURL)
-                        .setColor('RANDOM')
-                        .addField('**Time Taken:**',msg + " ms 📶 ")
-                        .addField('**WebSocket:**',api + " ms 📶 ")
-         message.channel.send({embed:embed});
-                        }
-                    });
-	}
-
-	function log(logmessage) {
-		if (message.guild.channels.has(logChannel)) {
-			message.guild.channels.get(logChannel).send({ embed: logmessage}).then().catch(err => console.log(err));
-		}
-	}
-
-	// set the max mentions/links/attachments that are allowed
-	let banLevel = {
-		"mentions": 10,
-		"links": 10,
-		"attachments": 10
-	};
-
-	// Ignore bots, DMs, Webhooks, if this bot has no perms, and Mods
-	if (message.author.bot || !message.guild || !message.member || !message.guild.member(client.user).hasPermission("BAN_MEMBERS") || message.member.hasPermission("MANAGE_MESSAGES")) return;
-
-	// Ignore if 1 mention and it's a bot (bot interaction)
-	if (message.mentions.users.size == 1 && message.mentions.users.first().bot) return;
-
-	// If there is no trace of the author in the slowmode map, add them.
-	let entry_mentions = slowmode_mentions.get(message.author.id);
-	let entry_links = slowmode_links.get(message.author.id);
-	let entry_attachments = slowmode_attachments.get(message.author.id);
-
-	if (!entry_mentions) {
-		entry_mentions = 0;
-		slowmode_mentions.set(message.author.id, entry_mentions);
-	}
-	if (!entry_links) {
-		entry_links = 0;
-		slowmode_links.set(message.author.id, entry_links);
-	}
-	if (!entry_attachments) {
-		entry_attachments = 0;
-		slowmode_attachments.set(message.author.id, entry_attachments);
-	}
-
-	// Count the unique user and roles mentions, links and attachments
-	entry_mentions += message.mentions.users.size + message.mentions.roles.size;
-	entry_links += message.embeds.length;
-	entry_attachments += message.attachments.size;
-	// Set all the amounts in the slowmode maps
-	slowmode_mentions.set(message.author.id, entry_mentions);
-	slowmode_links.set(message.author.id, entry_links);
-	slowmode_attachments.set(message.author.id, entry_attachments);
-
-	// If the total number of links in the last ratelimit is above the server ban level, ban user
-	if (entry_links > banLevel.links) {
-		message.member.ban(1).then(member => {
-			message.channel.send(`:ok_hand: banned \`${message.author.tag}\` for \`link spam\``);
-			log(new Discord.RichEmbed().setTitle(':hammer: Banned').setColor(0xFF0000).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Posting too many links (${entry_links}x)`));
-			slowmode_links.delete(message.author.id);
-		})
-		.catch(e => {
-			log(new Discord.RichEmbed().setTitle(':x: ERROR').setColor(0x000001).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Could not ban because they have a higher role`));
-		});
-	} else {
-		setTimeout(()=> {
-			entry_links -= message.embeds.length;
-			if(entry_links <= 0) slowmode_links.delete(message.author.id);
-		}, ratelimit);
-	}
-
-	if (entry_mentions > banLevel.mentions) {
-		message.member.ban(1).then(member => {
-			message.channel.send(`:ok_hand: banned \`${message.author.tag}\` for \`mention spam\``);
-			log(new Discord.RichEmbed().setTitle(':hammer: Banned').setColor(0xFF0000).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Mentioning too many users (${entry_mentions}x)`));
-			slowmode_mentions.delete(message.author.id);
-		})
-		.catch(e => {
-			log(new Discord.RichEmbed().setTitle(':x: ERROR').setColor(0x000001).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Could not ban because they have a higher role`));
-		});
-	} else {
-		setTimeout(()=> {
-			entry_mentions -= message.mentions.users.size + message.mentions.roles.size;
-			if(entry_mentions <= 0) slowmode_mentions.delete(message.author.id);
-		}, ratelimit);
-	}
-
-	if (entry_attachments > banLevel.attachments) {
-		message.member.ban(1).then(member => {
-			message.channel.send(`:ok_hand: banned \`${message.author.tag}\` for \`image spam\``);
-			log(new Discord.RichEmbed().setTitle(':hammer: Banned').setColor(0xFF0000).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Posting too many images (${entry_attachments}x)`));
-			slowmode_attachments.delete(message.author.id);
-		})
-		.catch(e => {
-			log(new Discord.RichEmbed().setTitle(':x: ERROR').setColor(0x000001).setTimestamp().addField('User', `${message.author.tag} (${message.author.id})`).addField('Reason', `Could not ban because they have a higher role`));
-		});
-	} else {
-		setTimeout(()=> {
-			entry_attachments -= message.attachments.size;
-			if(entry_attachments <= 0) slowmode_attachments.delete(message.author.id);
-		}, ratelimit);
-	}
-
-});
-
-process.on("unhandledRejection", err => {
-	console.error("Uncaught Promise Error: \n" + err.stack);
-});
-
-
-
-
 
 
 
