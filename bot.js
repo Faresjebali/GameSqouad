c040c1ff114faf881dd1077b25136037171380ac
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-07 04:53:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var blacklisted = ["discord.gg", "discord.gg/", "https://discord.gg"];
-const dateformat = require("dateformat");
+
 
 
 
@@ -99,27 +99,7 @@ client.on('message', message => {
         message.channel.sendEmbed(embed);
     }
 });
-client.on('message',function(message) {
-  var prefix = "*"
-  if(message.content.startsWith (prefix +'server')) {
-    const millis = new Date().getTime() - message.guild.createdAt.getTime();
-    const noww = new Date();
-    dateformat(noww, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
-    const createdAt = millis / 1000 / 60 / 60 / 24;
-    let embed = new Discord.RichEmbed()
-    .setThumbnail(message.author.avatarURL)
-    .addField(`${message.guild.name}`,`\`\`${createdAt.toFixed(0)} days ago \`\``)
-    .addField(':globe_with_meridians:** Server type**',`[** __${message.guild.region}__ **]`,true)
-    .addField(':medal:** __Roles__**',`[** __${message.guild.roles.size}__ **]`,true)
-    .addField(':red_circle:**__ Number of members __**',`[** __${message.guild.memberCount}__ **]`,true)
-    .addField(':large_blue_circle:**__ Number of members online __**',`[** __${message.guild.members.filter(m=>m.presence.status == 'online').size}__ **]`,true)
-    .addField(':pencil:**__  Text channels __**',`[** __${message.guild.channels.filter(m => m.type === 'text').size}__** ]`,true)
-    .addField(':microphone:**__  Voice channels __**',`[** __${message.guild.channels.filter(m => m.type === 'voice').size}__ **]`,true)
-    .addField(':crown:**__  Owner __**',`**${message.guild.owner}**`,true)
-    .addField(':id:**__  Owner ID __**',`**${message.guild.id}**`,true)
-    message.channel.send({embed:embed});
-  }
-});
+
 //,  , 
 client.on('guildMemberAdd', member => {
     var embed = new Discord.RichEmbed()
