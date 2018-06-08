fcbd33be3b2f5a2b1a22dd21f736f2fa06a2f01d
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-08 02:28:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -628,11 +628,11 @@ if(message.content.startsWith(prefix + 'botinfo')) {
 client.on('message', message => {
 	if(message.content.startsWith(prefix + 'purge')) {
 	if(!message.member.hasPermission("MANAGE_MESSAGES")) return errors.noPerms(message, "MANAGE_MESSAGES");
-  if(!args[0]) return message.channel.send("oof");
+  if(!args[0]) return message.channel.send("Are you sure about that? :thinking: ");
   message.channel.bulkDelete(args[0]).then(() => {
     message.channel.send(`Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
             }
-	}				    
+	};				    
   });
 
 
