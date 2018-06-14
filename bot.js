b315eb495c4dcec4f8dfb7ea254e1cb0bab14a2c
Faresjebali <38439810+Faresjebali@users.noreply.github.com>
2018-06-14 15:21:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,7 @@ client.on('message', message => {
       .setTitle("**List of Commands**\n") // sets the title to List of Commands
             .addField(" - help", "Displays this message (Correct usage: *help)") // sets the first field to explain the command *help
             .addField(" - info", "Tells info about myself :grin:") // sets the field information about the command *info
+            .addField(" -stats","Shows The Bot stats")
             .addField(" - ping", "Tests your ping (Correct usage: *ping)")
             .addField(" -Report","Report a member (correct usage : *report @person reason ")
             .addField(" - avatar","It will get your Discord Picture")
@@ -612,5 +613,24 @@ client.on('message', msg => {
     }
 }
 });
+client.on('message' , async (message) => {
+  var prefix = "*"
+ if (message.content.startsWith(prefix + 'stats')) {
+ const os = require('os');
+    const arch = os.arch()
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
 
+    let totalSeconds = process.uptime();
+    let realTotalSecs = Math.floor(totalSeconds % 60);
+    let days = Math.floor((totalSeconds % 31536000) / 86400);
+    let hours = Math.floor((totalSeconds / 3600) % 24);
+    let mins = Math.floor((totalSeconds / 60) % 60);
+
+    var ping = client.ping
+    message.channel.send(`\n= Memory usage: ${Math.round(used * 100) / 100}MB\n= Ping: ${ping}\n= Uptime: Days: ${days} | Hours: ${hours} | Minutes: ${mins} | Seconds: ${realTotalSecs}\n= Node: ${process.version}\n= Library: discord.js\n= ARCH: ${arch}\n= Plataform: ${os.platform}\n= Servers: ${client.guilds.size}\n= Users: ${client.users.size}`, {
+        code: 'AsciiDoc'
+    })
+
+}
+});
 client.login('NDUyMzEwMDUwNjAyNzQ1ODU4.DfOebw.LRgLqyrjEV8BGIm24IU7_pcqCQk');
